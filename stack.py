c8ae1de2b0dc1f241b0ff59314740ca43aa724ed
Israel Matos <arstotz.1803@outlook.com>
2022-01-26 07:14:13 -0300

    Updated code

--- stack.py
+++ stack.py
@@ -1,11 +1,10 @@
 import telebot
 import os
+#I will leave it the way it is for now, im tired
 
 #bot API token and other stuff
-API = "5137897681:AAGNSIgRqLbD0gvzNgvcJVviE9SVxJerNOs"
-#admin =
-#channel =
-arr = ["APPLE", "HOME", "ANON"]
+API = os.getenv("API")
+
 #bot object initialization
 bot = telebot.TeleBot(API)
 
@@ -14,9 +13,9 @@ bot = telebot.TeleBot(API)
 def Start(message):
     bot.send_message(message.chat.id, "Welcome! I will help you find questions and answers in StackOverflow! Put @FStackOverflowBot in command line I will show you the results accordingly.")
 
+#Inline query
 @bot.inline_handler(lambda query: query.query == arr)
 def Inline(inline_query):
-    print(True)
-
+    return
 
 bot.polling()
